c49b8d8f9dab966bde3108df073a5ab0fb4a68fd
NiclasNorin <niclas.norin@frontwalker.se>
2023-02-14 12:47:52 +0100

    render liked

--- source/js/front/render.js
+++ source/js/front/render.js
@@ -8,15 +8,16 @@ class Render {
         const containers = document.querySelectorAll('[js-like-container]');
         if(posts && posts.length > 0 && containers) {
             containers.forEach(container => {
+                let likeButtons = [];
                 posts.forEach(post => {
                     console.log(post);
                     const childElement = document.createElement('div');
-                    const html = this.components.card.html.replace('{LIKE_POST_TITLE}', post.title.rendered).replace('{LIKE_POST_CONTENT}', post.excerpt.rendered).replace('{LIKE_POST_ID}', post.id).replace('{LIKE_POST_LINK', post.link); 
-                    console.log(html);
+                    const html = this.components.card.html.replace('{LIKE_POST_TITLE}', post.title.rendered).replace('{LIKE_POST_CONTENT}', post.excerpt.rendered).replace('{LIKE_POST_ID}', post.id).replace('{LIKE_POST_LINK', post.link).replace('{LIKE_POST_IMAGE}', post.image ? post.image.source_url : ''); 
                     childElement.innerHTML = html;
                     container.appendChild(childElement);
+                    likeButtons.push(childElement.querySelector('[data-like-icon]'));
                 });
-                this.likeInstance.setListeners();
+                this.likeInstance.setListeners(likeButtons);
             })
         }
     }
