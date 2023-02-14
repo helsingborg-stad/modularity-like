5e9207cf4dad22003db062b9fc265f039030dcfd
NiclasNorin <niclas.norin@frontwalker.se>
2023-02-14 12:43:45 +0100

    Get the media

--- source/js/front/getPosts.js
+++ source/js/front/getPosts.js
@@ -35,9 +35,18 @@ class GetPosts {
         filteredPosts.forEach(post => {
             if (post._embedded['wp:featuredmedia']) {
                 let featuredImageId = post._embedded['wp:featuredmedia'][0].id;
-                let featuredImageUrl = post._embedded['wp:featuredmedia'][0];
+
+                if((post._embedded['wp:featuredmedia'][0].media_details)) {
+                    let featuredImageUrl;
+                    featuredImageUrl = post._embedded['wp:featuredmedia'][0].media_details.sizes.medium;
+                    console.log(featuredImageUrl);
+                    post.image = featuredImageUrl;
+                };
+
             }
         });
+
+        console.log(filteredPosts);
         this.renderInstance.renderComponents(filteredPosts);
     }
 }
