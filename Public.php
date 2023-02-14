16de54a099998ae56a61697bac9e8a9f1467aa16
NiclasNorin <niclas.norin@frontwalker.se>
2023-02-14 10:07:49 +0100

    public

---
+++ Public.php
@@ -0,0 +1,50 @@
+<?php
+
+// Public functions
+
+
+use ComponentLibrary\Init as ComponentLibraryInit;
+
+if (!function_exists('liked_posts_render_blade_view')) {
+    function liked_posts_render_blade_view($view, $data = [], $compress = true)
+    {
+        $init = new ComponentLibraryInit([
+            MODULARITYLIKEPOSTS_VIEW_PATH,
+          
+        ]);
+
+        $bladeEngine = $init->getEngine();
+
+        try {
+            $markup = $bladeEngine->make(
+                $view,
+                array_merge(
+                    $data,
+                    array('errorMessage' => false)
+                )
+            )->render();
+        } catch (\Throwable $e) {
+            $markup .= '<pre style="border: 3px solid #f00; padding: 10px;">';
+            $markup .= '<strong>' . $e->getMessage() . '</strong>';
+            $markup .= '<hr style="background: #000; outline: none; border:none; display: block; height: 1px;"/>';
+            $markup .= $e->getTraceAsString();
+            $markup .= '</pre>';
+        }
+
+        if ($compress == true) {
+            $replacements = array(
+              ["~<!--(.*?)-->~s", ""],
+              ["/\r|\n/", ""],
+              ["!\s+!", " "]
+            );
+
+            foreach ($replacements as $replacement) {
+                $markup = preg_replace($replacement[0], $replacement[1], $markup);
+            }
+
+            return $markup;
+        }
+
+        return $markup;
+    }
+}
