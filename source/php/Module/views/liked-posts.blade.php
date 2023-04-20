f1ef6026337d421c7b3ce3281424c8e7115c74f2
NiclasNorin <niclas.norin@frontwalker.se>
2023-04-20 10:09:51 +0200

    like icon animation

--- source/php/Module/views/liked-posts.blade.php
+++ source/php/Module/views/liked-posts.blade.php
@@ -13,6 +13,9 @@
     'classList' => ['c-collection--posts', 'o-grid'],
     'attributeList' => ['js-like-container' => '', 'js-display-as' => $display_as, 'js-post-types' => $postTypes, 'js-columns' => $postColumns, 'js-like-emblem-url' => $emblem],
     ])
+    @for ($i = 0; $i < 3; $i++)
+        <div style="height:400px;width:100%;" class="liked-posts__preloader u-preloader u-preloader__opacity--7 u-rounded o-grid-4@md"></div>
+    @endfor
 @endcollection
 @else
 <div class="o-grid" js-like-container js-display-as="{{$display_as}}" js-like-emblem-url="{{$emblem}}" js-post-types="{{$postTypes}}" js-columns="{{$postColumns}}">
