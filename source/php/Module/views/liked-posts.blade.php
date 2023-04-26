745dbe771177d8e6a458fc55e37de6b42e70a3ac
NiclasNorin <niclas.norin@frontwalker.se>
2023-04-26 16:49:29 +0200

    container wrapper and better preloader

--- source/php/Module/views/liked-posts.blade.php
+++ source/php/Module/views/liked-posts.blade.php
@@ -1,3 +1,4 @@
+<div class="container">
 @if (!$hideTitle && !empty($postTitle))
     <div class="liked-posts__header">
         @typography([
@@ -14,14 +15,16 @@
     'attributeList' => ['js-like-container' => '', 'js-display-as' => $display_as, 'js-post-types' => $postTypes, 'js-columns' => $postColumns, 'js-like-emblem-url' => $emblem],
     ])
     @for ($i = 0; $i < 3; $i++)
-        <div style="height:400px;width:100%;" class="liked-posts__preloader u-preloader u-preloader__opacity--7 u-rounded o-grid-4@md"></div>
+        <div style="height:170px;width:100%;" class="liked-posts__preloader u-preloader u-preloader__opacity--7 u-rounded {{$postColumns}}"></div>
     @endfor
 @endcollection
 @else
-<div class="o-grid" js-like-container js-display-as="{{$display_as}}" js-like-emblem-url="{{$emblem}}" js-post-types="{{$postTypes}}" js-columns="{{$postColumns}}">
-    @for ($i = 0; $i < 3; $i++)
-        <div style="height:400px;width:100%;" class="liked-posts__preloader u-preloader u-preloader__opacity--7 u-rounded o-grid-4@md"></div>
-    @endfor
-</div>
+    <div class="o-grid" js-like-container js-display-as="{{$display_as}}" js-like-emblem-url="{{$emblem}}" js-post-types="{{$postTypes}}" js-columns="{{$postColumns}}">
+        @for ($i = 0; $i < 3; $i++)
+            <div style="height:400px;width:100%;" class="liked-posts__preloader u-preloader u-preloader__opacity--7 u-rounded o-grid-4@md">
+            </div>
+        @endfor
+    </div>
 @endif
+</div>
 
