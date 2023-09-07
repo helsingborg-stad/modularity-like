a0ee1045837ce331a313b04885f42818b6e79e0e
Niclas Norin <niclas.norin@helsingborg.se>
2023-09-07 15:39:19 +0200

    Fix: Like posts error notice

--- source/php/App.php
+++ source/php/App.php
@@ -36,7 +36,7 @@ class App
     public function postsIcon($callToActionArray, $post)
     {
         $postTypes = get_field('select_post_type', 'option') ?? [];
-        if (in_array($post->post_type, $postTypes)) {
+        if (!empty($post->post_type) && in_array($post->post_type, $postTypes)) {
             $callToActionArray['floating'] =  ['icon' => 'favorite', 'filled' => false, 'size' => 'md', 'attributeList' => ['data-like-icon' => '', 'data-post-id' => $post->ID, 'data-post-type' => $post->post_type], 'classList' => ['like-icon'], 'postTypes' => $postTypes];
         };
 
