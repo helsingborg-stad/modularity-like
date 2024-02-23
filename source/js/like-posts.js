52ac4c0604d0f0193c7a523e109f4bb366adbc7f
Niclas <niclasn@outlook.com>
2024-02-23 12:32:34 +0100

    chore: remove .log

--- source/js/like-posts.js
+++ source/js/like-posts.js
@@ -3,7 +3,6 @@ import Like from './front/like';
 import { initializeLikedCounter } from "./front/likedCounter";
 import { initializeShare } from './front/share';
 
-console.log(likedPostsComponents);
 initializeLikedCounter(likedPostsComponents.counterElement);
 const LikeInstance = new Like();
 const GetPostsInstance = new GetPosts(likedPostsComponents);
