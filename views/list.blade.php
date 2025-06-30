f1532b9b560e4ef2ac2c4b7d3d520fdcea2ae669
NiclasNorin <niclasn@outlook.com>
2025-06-30 15:01:35 +0200

    refactor: check empty posts crashing blade rendering

--- views/list.blade.php
+++ views/list.blade.php
@@ -1,36 +1,38 @@
-@foreach($posts as $post)
-    @collection__item([
-        'link' => $post->getPermalink(),
-        'displayIcon' => false,
-        'classList' => ['like-posts__list-item'],
-    ])
-    @slot('before')
-        @element([
-            'attributeList' => ['data-js-like-icon-wrapper' => ''],
+@if(!empty($posts))
+    @foreach($posts as $post)
+        @collection__item([
+            'link' => $post->getPermalink(),
+            'displayIcon' => false,
+            'classList' => ['like-posts__list-item'],
         ])
-            @icon([
-                'icon' => $icon, 
-                'size' => 'md',
-                'attributeList' => [
-                    'data-like-icon' => '', 
-                    'data-post-type' => $post->getPostType(), 
-                    'data-post-id' => $post->getId(),
-                    'data-blog-id' => $post->blogId
-                ],
-                'classList' => [
-                    'like-posts__list-icon',
-                ],
-                'filled' => true,
+        @slot('before')
+            @element([
+                'attributeList' => ['data-js-like-icon-wrapper' => ''],
             ])
-            @endicon
-        @endelement
-    @endslot
-        @typography([
-            'element' => 'h2',
-            'variant' => 'h4',
-            'useHeadingsContext' => false,
-        ])
-            {!! $post->getTitle() !!}
-        @endtypography
-    @endcollection__item
-@endforeach
+                @icon([
+                    'icon' => $icon, 
+                    'size' => 'md',
+                    'attributeList' => [
+                        'data-like-icon' => '', 
+                        'data-post-type' => $post->getPostType(), 
+                        'data-post-id' => $post->getId(),
+                        'data-blog-id' => $post->blogId
+                    ],
+                    'classList' => [
+                        'like-posts__list-icon',
+                    ],
+                    'filled' => true,
+                ])
+                @endicon
+            @endelement
+        @endslot
+            @typography([
+                'element' => 'h2',
+                'variant' => 'h4',
+                'useHeadingsContext' => false,
+            ])
+                {!! $post->getTitle() !!}
+            @endtypography
+        @endcollection__item
+    @endforeach
+@endif
