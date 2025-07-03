566afdffd5fa9020ff6d0efe4c0c71606643c35e
NiclasNorin <niclasn@outlook.com>
2025-07-03 15:19:59 +0200

    fix: corrected getting user id

--- views/list.blade.php
+++ views/list.blade.php
@@ -1,38 +1,40 @@
 @if(!empty($posts))
     @foreach($posts as $post)
-        @collection__item([
-            'link' => $post->getPermalink(),
-            'displayIcon' => false,
-            'classList' => ['like-posts__list-item'],
-        ])
-        @slot('before')
-            @element([
-                'attributeList' => ['data-js-like-icon-wrapper' => ''],
+        @if(!empty($post))
+            @collection__item([
+                'link' => $post->getPermalink(),
+                'displayIcon' => false,
+                'classList' => ['like-posts__list-item'],
             ])
-                @icon([
-                    'icon' => $icon, 
-                    'size' => 'md',
-                    'attributeList' => [
-                        'data-like-icon' => '', 
-                        'data-post-type' => $post->getPostType(), 
-                        'data-post-id' => $post->getId(),
-                        'data-blog-id' => $post->blogId
-                    ],
-                    'classList' => [
-                        'like-posts__list-icon',
-                    ],
-                    'filled' => true,
+            @slot('before')
+                @element([
+                    'attributeList' => ['data-js-like-icon-wrapper' => ''],
                 ])
-                @endicon
-            @endelement
-        @endslot
-            @typography([
-                'element' => 'h2',
-                'variant' => 'h4',
-                'useHeadingsContext' => false,
-            ])
-                {!! $post->getTitle() !!}
-            @endtypography
-        @endcollection__item
+                    @icon([
+                        'icon' => $icon, 
+                        'size' => 'md',
+                        'attributeList' => [
+                            'data-like-icon' => '', 
+                            'data-post-type' => $post->getPostType(), 
+                            'data-post-id' => $post->getId(),
+                            'data-blog-id' => $post->blogId
+                        ],
+                        'classList' => [
+                            'like-posts__list-icon',
+                        ],
+                        'filled' => true,
+                    ])
+                    @endicon
+                @endelement
+            @endslot
+                @typography([
+                    'element' => 'h2',
+                    'variant' => 'h4',
+                    'useHeadingsContext' => false,
+                ])
+                    {!! $post->getTitle() !!}
+                @endtypography
+            @endcollection__item
+        @endif
     @endforeach
 @endif
