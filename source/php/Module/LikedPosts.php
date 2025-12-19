7965815cd1b298bac707a93ccd6bafd80026e1d2
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-12-19 14:29:26 +0100

    fix: improve readability on module code.

--- source/php/Module/LikedPosts.php
+++ source/php/Module/LikedPosts.php
@@ -4,6 +4,10 @@ namespace ModularityLikePosts\Module;
 
 use ModularityLikePosts\Module\SharedPosts;
 use ModularityLikePosts\Helper\GetOptionFields;
+use AcfService\AcfService;
+use AcfService\Implementations\NativeAcfService;
+use \WpService\Implementations\WpServiceWithTypecastedReturns;
+use \WpService\Implementations\NativeWpService;
 
 class LikedPosts extends \Modularity\Module
 {
@@ -11,6 +15,8 @@ class LikedPosts extends \Modularity\Module
     public $supports = array();
     private $template = 'liked-posts';
     private $getOptionFieldsHelper;
+    private $acfService;
+    private $wpService;
 
     public function init()
     {
@@ -19,7 +25,11 @@ class LikedPosts extends \Modularity\Module
         $this->namePlural = __("Liked posts", 'modularity-like');
         $this->description = __("Shows the users liked posts", 'modularity-like');
 
-        $this->getOptionFieldsHelper = new GetOptionFields();
+        //Initialize services, helpers etc.
+        $this->acfService            = new NativeAcfService();
+        $this->wpService             = new WpServiceWithTypecastedReturns(new NativeWpService());
+        $this->getOptionFieldsHelper = new GetOptionFields($this->acfService);
+
         $this->registerMeta();
     }
 
@@ -29,38 +39,66 @@ class LikedPosts extends \Modularity\Module
      */
     public function data(): array
     {
-        $fields = get_fields($this->ID);
+        $fields = $this->getFields();
       
-        $data['likeIcon'] = $this->getOptionFieldsHelper->getIcon();
-        $data['noPostsFound'] = 
-            get_field('like_no_posts_found_text', 'option') ?: 
-            __('No liked posts were found', 'modularity-like');
-
-        /* Possibility to add more views */
-        $data['appearance'] = !empty($fields['liked_posts_appearance']) ? $fields['liked_posts_appearance'] : 'collection';
-
-        $data['postTypes'] = json_encode($fields['liked_post_types_to_show']);
-        $data['postColumns'] = apply_filters('Modularity/Display/replaceGrid', $fields['liked_posts_columns']);
-        $data['shareButton'] = $fields['liked_posts_share_button'];
-        $data['labels'] = [
-            'shareButtonLabel' => __('Share favorites', 'modularity-like'),
-            'shareSuccess' => __('Link was copied successfully', 'modularity-like'),
-            'shareError' => __('Something went wrong, link: ', 'modularity-like'),
-            'close' => __('Close', 'modularity-like'),
-            'shareLinkLabel' => __('Share your link', 'modularity-like'),
-            'shareLinkName' => __('List name', 'modularity-like'), 
-            'shareLinkExcerpt' => __('List excerpt', 'modularity-like'), 
-        ];
-        $data['id'] = uniqid();
+        $data['likeIcon']     = $this->getOptionFieldsHelper->getIcon();
+        $data['noPostsFound'] = $this->getLabelNoPostsFound();
+        $data['appearance']   = !empty($fields['liked_posts_appearance']) ? $fields['liked_posts_appearance'] : 'collection';
+        $data['postTypes']    = json_encode($fields['liked_post_types_to_show']);
+        $data['postColumns']  = apply_filters('Modularity/Display/replaceGrid', $fields['liked_posts_columns']);
+        $data['shareButton']  = $fields['liked_posts_share_button'];
+        $data['labels']       = $this->getLabels();
+        $data['id']           = uniqid();
 
         return $data;
     }
 
+    /**
+     * Get the label for no posts found from ACF options.
+     *
+     * @return string The label for no posts found.
+     */
+    private function getLabelNoPostsFound(): string
+    {
+        return $this->acfService->getField(
+            'like_no_posts_found_text',
+            'option'
+        ) ?: __('No liked posts were found', 'modularity-like');
+    }
+
+    /**
+     * Get the labels used in the module.
+     *
+     * @return array An associative array of labels.
+     */
+    private function getLabels()
+    {
+        return [
+            'shareButtonLabel' => __('Share favorites', 'modularity-like'),
+            'shareSuccess'     => __('Link was copied successfully', 'modularity-like'),
+            'shareError'       => __('Something went wrong, link: ', 'modularity-like'),
+            'close'            => __('Close', 'modularity-like'),
+            'shareLinkLabel'   => __('Share your link', 'modularity-like'),
+            'shareLinkName'    => __('List name', 'modularity-like'), 
+            'shareLinkExcerpt' => __('List excerpt', 'modularity-like'), 
+        ];
+    }
+
+    /**
+     * Get the template file for the module.
+     *
+     * @return string The template file name.
+     */
     public function template(): string
     {
         return $this->template . ".blade.php";
     }
 
+    /**
+     * Register the user meta for liked posts.
+     *
+     * @return void
+     */
     private function registerMeta(): void
     {
         register_meta('user', 'likedPosts', array(
