beb3764a50994b33f6e79aa5eabede19169f2a5f
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-01-27 15:21:48 +0100

    fix: scoped patchers

--- scoper.inc.php
+++ scoper.inc.php
@@ -14,4 +14,16 @@ return [
     'exclude-namespaces' => [
         'Composer\\',
     ],
+    'patchers' => [
+        function (string $filePath, string $prefix, string $content): string {
+            if (strpos($filePath, 'WpService') !== false) {
+                $content = preg_replace('/namespace\\s+' . preg_quote($prefix, '/') . '\\WpService\\/', 'namespace ' . $prefix . '\\WpService\\', $content);
+                $content = preg_replace('/use\\s+' . preg_quote($prefix, '/') . '\\WpService\\/', 'use ' . $prefix . '\\WpService\\', $content);
+            }
+            if (strpos($content, '\\WpService\\') !== false && strpos($content, '\\' . $prefix . '\\WpService\\') === false) {
+                $content = str_replace('\\WpService\\', '\\' . $prefix . '\\WpService\\', $content);
+            }
+            return $content;
+        },
+    ],
 ];
