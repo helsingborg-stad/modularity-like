732b9ce4a6de11f72a696e19ae41935c41dfd6dc
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-01-27 15:02:07 +0100

    fix: minimize scoper config.

--- scoper.inc.php
+++ scoper.inc.php
@@ -4,36 +4,10 @@ use Isolated\Symfony\Component\Finder\Finder;
 
 return [
     'prefix' => 'ModularityLikePosts\\Dependencies',
-
     'finders' => [
         Finder::create()->files()->in('vendor')->name('*.php'),
     ],
-
-    'patchers' => [
-        // Add custom patchers here if needed
-    ],
-
     'expose-classes' => [
         'ModularityLikePosts\\*',
-    ],
-
-    'exclude-namespaces' => [
-        // Add namespaces to exclude from prefixing
-    ],
-
-    'exclude-files' => [
-        // Add specific files to exclude from prefixing
-    ],
-
-    'exclude-classes' => [
-        // Add specific classes to exclude from prefixing
-    ],
-
-    'exclude-functions' => [
-        // Add specific functions to exclude from prefixing
-    ],
-
-    'exclude-constants' => [
-        // Add specific constants to exclude from prefixing
     ],
 ];
