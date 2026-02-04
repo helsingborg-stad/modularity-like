02e6639c9664ce89e001eabc488e5135f3364d8f
GitHub Action <>
2026-02-04 08:37:41 +0000

    build: update package file versions to 3.1.2

--- like-posts.php
+++ like-posts.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Modularity Like Posts
  * Plugin URI:        https://github.com/helsingborg-stad/modularity-like
  * Description:       Lets users like and save posts.
- * Version: 3.1.0
+ * Version: 3.1.2
  * Author:            Niclas Norin
  * Author URI:        https://github.com/NiclasNorin
  * License:           MIT
